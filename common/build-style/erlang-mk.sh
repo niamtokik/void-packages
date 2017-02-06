baeda63197fae369b6fbc7373d4043b53c7424ee
mk <mk@steepath.eu>
2017-02-06 08:57:27 +0100

    build-style erlang-mk works. added erlang-cowlib to test it and works too.

--- common/build-style/erlang-mk.sh
+++ common/build-style/erlang-mk.sh
@@ -1,3 +1,19 @@
 #
 # helper package for erlang project using erlang.mk
 #
+
+do_configure() {
+	:
+}
+
+do_build() {
+	make all
+}
+
+do_install() {
+	ls -lhart
+	vmkdir usr/lib/erlang/lib/${pkgname}-${version}/ebin
+	vmkdir usr/lib/erlang/lib/${pkgname}-${version}/src
+	vcopy ebin/* usr/lib/erlang/lib/${pkgname}-${version}/ebin
+	vcopy src/* usr/lib/erlang/lib/${pkgname}-${version}/src
+}
